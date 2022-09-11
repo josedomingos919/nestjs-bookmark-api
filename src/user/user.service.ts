5e586d7dd51bdb52ac9c078db1c9023a3b96a138
josedomingos919 <josedomingos919@gmail.com>
2022-09-11 22:50:49 +0100

    feat: setting up user module

---
+++ src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { EditUserDto } from './dto';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async editUser(userId: number, dto: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+
+    delete user.password;
+
+    return user;
+  }
+}
